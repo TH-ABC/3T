cc85ebd6adc519414e467bca2f5f16b2b23d89fc
TH <ntrieuhoang93@gmail.com>
2025-12-03 19:30:13 +0700

    Update OrderList.tsx

--- Version 3.03/components/OrderList.tsx
+++ Version 3.03/components/OrderList.tsx
@@ -1017,7 +1017,7 @@ export const OrderList: React.FC<OrderListProps> = ({ user, onProcessStart, onPr
                     <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse gap-2">
                         {sysModal.type === 'confirm' ? (
                             <>
-                                <button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm" onClick={() => { sysModal.onConfirm && sysModal.onConfirm(); closeMessage(); }}>
+                                <button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm" onClick={() => { sysModal.onConfirm?.(); closeMessage(); }}>
                                     Xác Nhận
                                 </button>
                                 <button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onClick={closeMessage}>
